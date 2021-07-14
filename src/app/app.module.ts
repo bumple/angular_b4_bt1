d60f95307c2af5aaa0e25c163ed88416ccdeb82d
thanhdeptrai <thanh.n5151@gmail.com>
2021-07-14 16:57:29 +0700

    thanh

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ArticleComponent } from './article/article.component';
+import {FormsModule} from "@angular/forms";
+import { LikeComponent } from './like/like.component';
+import {Ng2SearchPipeModule} from "ng2-search-filter";
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleComponent,
+    LikeComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    Ng2SearchPipeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
